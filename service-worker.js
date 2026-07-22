0835bad08afc68446d990b5a69ceb4f04d77bd89
Martin_MMC <martin.poliyanskiy+github@gmail.com>
2026-07-21 21:06:51 -0700

    Update service worker cache name and structure

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,42 @@
-const CACHE_NAME = "my-site-cache-v1";
+const CACHE_NAME = "marty-games-cache-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/404.html",
-  "/favicon.ico",
-  "/domains.json",
-  "/manifest.json",
-  "/zones/zones.json"
+    "/",
+    "/index.html",
+    "/404.html",
+    "/favicon.ico",
+    "/domains.json",
+    "/manifest.json"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    // ignoreSearch: true ensures /?utm_source=pwa matches the cached "/"
-    caches.match(event.request, { ignoreSearch: true }).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request, {
+            ignoreSearch: true
+        }).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
